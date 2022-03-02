0532aede508e17a91fd6e15a2298af76cf290f48
vukasinovic21 <nikolavukasinovic8@gmail.com>
2022-03-02 19:12:35 +0100

    mini projekat
    Dodavanje mog dela mini projekta

---
+++ Sandbox/Nikola Vukasinovic/back/StudentiAPi/ImePredmeta.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentiAPi
+{
+    public class ImePredmeta
+    {
+        public int Id { get; set; }
+        [StringLength(20)]
+
+        public string IPredmet { get; set; } = string.Empty;
+    }
+}
---
+++ Sandbox/Nikola Vukasinovic/back/StudentiAPi/Migrations/20220301183131_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentiAPi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ImenaPredmeta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IPredmet = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImenaPredmeta", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StatusOpcija = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Predmeti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Komentar = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ImePredmetaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Predmeti", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Predmeti_ImenaPredmeta_ImePredmetaId",
+                        column: x => x.ImePredmetaId,
+                        principalTable: "ImenaPredmeta",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Predmeti_ImePredmetaId",
+                table: "Predmeti",
+                column: "ImePredmetaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Predmeti");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropTable(
+                name: "ImenaPredmeta");
+        }
+    }
+}
