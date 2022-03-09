3ce777e7e5c3692e0c059e76923497e93a548943
Tijana Đoković <tdjokovic00@gmail.com>
2022-03-09 19:45:32 +0100

    trained sequential model ann

--- src/ML/pythonML.py
+++ src/ML/pythonML.py
@@ -1,86 +1,133 @@
-import pandas as pd
-import pathlib
-import numpy as np
-import seaborn as sns
 import tensorflow as tf
+from tensorflow import keras
+from keras import Sequential
+
+from keras.layers import Conv1D, MaxPool1D, Flatten, Dense, Dropout, BatchNormalization
+from keras.optimizer_v1 import Adam
+print(tf.__version__)
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-from tensorflow import keras
-from keras import layers
-
-
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import VarianceThreshold
 
-from keras import Sequential
-from keras import models
+# dataset
+data = pd.read_csv("./src/ML/train.csv")
+data.head()
+data.info
 
-#import tensorflow_docs as tfdocs
-#import tensorflow_docs.plots
-#import tensorflow_docs.modeling
+data.shape
 
+X = data.drop(labels=['ID', 'TARGET'], axis=1)
+X.shape
 
+y = data['TARGET']
 
-
-# load data from csv file
-wine_quality = pd.read_csv("./src/ML/WineQT.csv", index_col=0)
-
-# print data info and column headers
-print(wine_quality.head())
-print(wine_quality.info())
-
-# checking if there is any NA columns
-print(wine_quality.isna().sum()) if (wine_quality.isna().sum().any() > 0) else print("Data is clean")
-
-# checking mean quality grade
-print("Mean value of wine quality is ",wine_quality['quality'].mean())
-
-# splitting the train and test dataset
-# train dataset is 80% of the whole dataset
-train_wd = wine_quality.sample(frac=0.8, random_state=0)
-test_wd = wine_quality.drop(train_wd.index)
-
-# making train and test dataset labels without quality column
-train_labels = train_wd.pop('quality')
-test_labels = test_wd.pop('quality')
-
-# normalizing train and test data
-normed_train_data = normalize(train_wd)
-normed_test_data = normalize(test_wd)
-
-# making function for sequential model for training
-# model with 5 layers and relu activation function
-# number of neurons : 64, 128, 32, 16, 1
-def build_model():
-    model = Sequential([
-        layers.Dense(64,activation="relu",input_shape=[len(train_wd.keys())], name="layer1"),
-        layers.Dense(128,activation="relu",name="layer2"),
-        layers.Dense(32,activation="relu",name="layer3"),
-        layers.Dense(16,activation="relu",name="layer4"),
-        layers.Dense(1,name="layer5"), # this layer has to have only one neuron
-    ])
-
-    # optimizing the model
-    model.compile(loss='mse', optimizer='adam', metrics=['mae', 'mse'])
-    return model
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0, stratify=y)
 
 
+X_train.shape, X_test.shape
 
-"""
-# building the optimized model
-model = build_model()
+# sada uklanjamo kolone koje zapravo nemaju nikakvo znacenje
+# QUASI constants removal
 
-################################################################################
-# training the dataset using our built model
+constant_filter = VarianceThreshold(threshold=0)
+constant_filter.fit(X_train)
 
-# defining training with 1000 epochs
-EPOCHS = 1000
+constant_filter.get_support().sum()
 
-history = model.fit(
-    normed_train_data, train_labels,
-    epochs=EPOCHS, validation_split=0.2, verbose = 0
-)
-"""
+constant_list = [not temp for temp in constant_filter.get_support()]
+constant_list
 
-print("done")
+X.columns[constant_list]
+
+X_train_filter = constant_filter.transform(X_train)
+X_test_filter = constant_filter.transform(X_test)
+
+X_train_filter.shape, X_test_filter.shape, X_train.shape
+
+# Quasi removal
+
+quasi_constant_filter = VarianceThreshold(threshold=0.01)
+
+quasi_constant_filter.fit(X_train_filter)
+quasi_constant_filter.get_support().sum()
+
+X_train_quasi_filter = quasi_constant_filter.transform(X_train_filter)
+X_test_quasi_filter = quasi_constant_filter.transform(X_test_filter)
+
+X_train_quasi_filter.shape, X_test_quasi_filter.shape
+
+# remove duplicate features
+X_train_T = X_train_quasi_filter.T
+X_test_T = X_test_quasi_filter.T
+
+type(X_train_T)
+
+X_train_T = pd.DataFrame(X_train_T)
+X_test_T = pd.DataFrame(X_test_T)
+
+X_train_T.shape, X_test_T.shape
+
+X_train_T.duplicated().sum()
+
+duplicated_features = X_train_T.duplicated()
+duplicated_features
+
+features_to_keep = [not index for index in duplicated_features]
+features_to_keep
+
+X_train = X_train_T[features_to_keep].T
+X_train.shape
+
+X_test = X_test_T[features_to_keep].T
+X_test.shape
+
+X_train.head()
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+X_train.shape, X_test.shape
+
+X_train.reshape(8000,224,1)
+X_test.reshape(2000,224,1)
+
+y_train = y_train.to_numpy()
+y_test = y_test.to_numpy()
+
+####### BUILD CNN
+
+model = Sequential()
+model.add(Conv1D(32, 3, activation='relu', input_shape=(224,1)))
+model.add(BatchNormalization())
+model.add(MaxPool1D(2))
+model.add(Dropout(0.3))
+
+model.add(Conv1D(64, 3, activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool1D(2))
+model.add(Dropout(0.5))
+
+model.add(Conv1D(128, 3, activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool1D(2))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(224, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(1, activation='sigmoid'))
+
+model.summary()
+
+model.compile(optimizer = 'adam', loss='binary_crossentropy', metrics=['accuracy'] )
+history = model.fit(X_train, y_train, epochs=10, validation_data = (X_test, y_test), verbose=1)
+
+history.history
