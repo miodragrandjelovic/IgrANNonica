97798ffc9ab43191ce2c92fa9b2b6847aa29daa8
Miodrag Randjelovic <dobropamtimsve00@gmail.com>
2022-03-12 18:44:45 +0100

    deo posta

--- src/Backend/Backend/Controllers/PythonController.cs
+++ src/Backend/Backend/Controllers/PythonController.cs
@@ -19,5 +19,28 @@ namespace Backend.Controllers
 
             return Ok(users);
         }
+        /*
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> Post()
+        {
+
+            HttpResponseMessage httpResponse = await http.PostAsync("http://127.0.0.1:3000/users");
+            var users = JsonSerializer.Deserialize<List<UserDto>>(await httpResponse.Content.ReadAsStringAsync());
+
+            UserDto user1=new UserDto();
+            user1.FirstName = "pera";
+            user1.LastName = "perci";
+            user1.Username = "pera";
+            user1.Password = "sifra123";
+            /*UserDto user2=new UserDto();
+            user2.FirstName = "mika";
+            user2.LastName = "mikic";
+            user2.Username = "mika";
+            user2.Password = "sifra123";
+            return user1;
+            
+    }
+*/
+
     }
 }
--- src/ML/Flask.py
+++ src/ML/Flask.py
@@ -1,8 +1,12 @@
-
 from flask import Flask
-from flask import jsonify
+from flask import jsonify,request
 
 app = Flask(__name__)
+
+@app.route("/users",methods=['POST'])
+def postAllUsers():
+    users=request.get_json() 
+    return jsonify(users)
 
 @app.route("/users", methods=['GET'])
 def  getAllUsers():
@@ -11,6 +15,11 @@ def  getAllUsers():
     {"FirstName": "mika", "LastName": "mikic","Username": "mika", "Password": "sifra123"},
     {"FirstName": "milan", "LastName": "milanic","Username": "milan", "Password": "sifra123"}])
 
+
 #if(__name__=="main"):
 app.run(port = 3000)
 
+
+#[{"FirstName": "petar", "LastName": "peric","Username": "pera", "Password": "sifra123"},
+#    {"FirstName": "mika", "LastName": "mikic","Username": "mika", "Password": "sifra123"},
+#    {"FirstName": "milan", "LastName": "milanic","Username": "milan", "Password": "sifra123"}]
