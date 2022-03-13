0fdb20789da7f81149239f9507c96979030ab432
Strahinja Tuovic <tuovicstrahinja@gmail.com>
2022-03-13 19:33:07 +0100

    Prikazivanje csv u tabeli

--- src/Front/src/app/home/csv/csv.component.ts
+++ src/Front/src/app/home/csv/csv.component.ts
@@ -6,8 +6,8 @@ import { Component } from "@angular/core";
 })
 export class CsvComponent {
 
-    headingLines = [];
-    rowLines = [];
+    headingLines: any = [];
+    rowLines: any = [];
 
     fileUpload(files: any) {
         let fileList = (<HTMLInputElement>files.target).files;
@@ -20,9 +20,31 @@ export class CsvComponent {
             reader.onload = (e) => {
                 let csv: any = reader.result;
                 let allTextLines = [];
-                allTextLines = csv.split(/\r|\n|\r/);
+                allTextLines = csv.split('\n');
+                
+                let headers = allTextLines[0].split(/;|,/);
+                let data = headers;
+                let headersArray = [];
 
-                console.log(allTextLines)
+                for (let i = 0; i < headers.length; i++) {
+                    headersArray.push(data[i]);
+                }
+                this.headingLines.push(headersArray);
+
+                let rowsArray = [];
+
+                let length = allTextLines.length - 1;
+                
+                let rows = [];
+                for (let i = 1; i < length; i++) {
+                    rows.push(allTextLines[i].split(/;|,/));
+                }
+                length = rows.length;
+                for (let j = 0; j < length; j++) {
+                    rowsArray.push(rows[j]);
+                }
+                this.rowLines.push(rowsArray);
+                console.log(this.rowLines);
             }
         }
     }
