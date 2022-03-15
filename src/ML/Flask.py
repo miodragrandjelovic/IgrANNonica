84e0a7d8c44d2400e7ac83ed4031ca8c1536bb18
vukasinovic21 <nikolavukasinovic8@gmail.com>
2022-03-15 19:29:35 +0100

    izmenjen flask

--- src/ML/Flask.py
+++ src/ML/Flask.py
@@ -8,21 +8,46 @@ users=[{"FirstName": "petar", "LastName": "peric","Email": "e@gmail.com","Userna
     {"FirstName": "mika", "LastName": "mikic","Email": "e@gmail.com","Username": "mika", "Password": "sifra123"},
     {"FirstName": "milan", "LastName": "milanic","Email": "e@gmail.com","Username": "milan", "Password": "sifra123"}]
 
+hiperparametri=[{"EncodingType": "HotEncoding", "LearningRate": 0.03, "Activation": "Tanh", "Epoch": 3, "Layers": 1, "NeuronsLvl1": 1, "NeuronsLvl2": 1, "NeuronsLvl3": 1, "NeuronsLvl4": 1, "NeuronsLvl5": 1, "Ratio": 50, "BatchSize": 10, "Randomize": 1},
+                {"EncodingType": "ColdEncoding", "LearningRate": 0.01, "Activation": "Tanko", "Epoch": 2, "Layers": 0, "NeuronsLvl1": 0, "NeuronsLvl2": 1, "NeuronsLvl3": 1, "NeuronsLvl4": 1, "NeuronsLvl5": 1,"Ratio": 30, "BatchSize": 9, "Randomize": 1}]
 
+#json ne moze da procita jer su jednostruki navodnici, a ako se stave dvostruki kako da stavimo string izmedju jer string mora sa dvostrukim da krece i da se zavrsava?!
+csve = [{"csvdata": "{'PassengerId':1,'Survived':0,'Pclass':3,'Name':'Braund, Mr. Owen Harris','Sex':'male','Age':22,'SibSp':1,'Parch':0,'Ticket':'A/5 21171','Fare':7.25,'Cabin':'','Embarked':'S'},{'PassengerId':2,'Survived':1,'Pclass':1,'Name':'Cumings, Mrs. John Bradley (Florence Briggs Thayer)','Sex':'female','Age':38,'SibSp':1,'Parch':0,'Ticket':'PC 17599','Fare':71.2833,'Cabin':'C85','Embarked':C'}"}]
+#csve = [{"csvdata": "radiliovo"},
+#        {"csvdata": "neradi"}]
 #@app.route("/users",methods=['POST'])
 #def postAllUsers():
  #   users=request.get_json() 
 
-@app.route("/users", methods=["POST"])
+@app.route("/users", methods=["POST"]) #Primanje sa beka
 def post_student():
     user = request.get_json()
     users.append(user)
     return users
 
-
-@app.route("/users", methods=['GET'])
+@app.route("/users", methods=['GET']) #Slanje na bek
 def  getAllUsers():
     return jsonify(users)
+
+@app.route("/hp", methods=["POST"]) #Primanje HP sa beka
+def post_hp():
+    hp = request.get_json()
+    hiperparametri.append(hp)
+    return hiperparametri
+
+@app.route("/hp", methods=['GET']) #Slanje HP na bek
+def  getAllHps():
+    return jsonify(hiperparametri)
+
+@app.route("/csv", methods=["POST"]) #Primanje CSV sa beka
+def post_csv():
+    cs = request.get_json()
+    csve.append(cs)
+    return csve
+
+@app.route("/csv", methods=['GET']) #Slanje CSV na bek
+def  getCsv():
+    return jsonify(csve)
 
 
 #if(__name__=="main"):
