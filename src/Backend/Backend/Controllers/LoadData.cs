cead7e4ca0fe89a9da30061a03906342805f9017
Strahinja Tuovic <tuovicstrahinja@gmail.com>
2022-03-19 18:23:54 +0100

    Odradjeno slanje csv-a back-u

--- src/Backend/Backend/Controllers/LoadData.cs
+++ src/Backend/Backend/Controllers/LoadData.cs
@@ -48,10 +48,11 @@ namespace Backend.Controllers
             //var hiperparametars = JsonSerializer.Deserialize<Hiperparametri>(await response.Content.ReadAsStringAsync());
             return Ok(hiperjson);
         }
-        
+
         [HttpPost("csv")] //Slanje CSV na pajton
-        public async Task<ActionResult<string>> PostCsv([FromBody]string csve)
+        public async Task<ActionResult<DataLoad>> PostCsv([FromBody] DataLoad cs)
         {
+            string csve = cs.CsvData;
             var data = new StringContent(csve, System.Text.Encoding.UTF8, "application/json");
             var url = "http://127.0.0.1:3000/csv";
             var response = await http.PostAsync(url, data);
