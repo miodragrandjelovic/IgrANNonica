cfa16a97e7398bbc7d94b6f8ec753b085aca0e5e
vukasinovic21 <nikolavukasinovic8@gmail.com>
2022-03-20 22:47:58 +0100

    post metoda u flasku

--- src/ML/Flask.py
+++ src/ML/Flask.py
@@ -13,6 +13,8 @@ hiperparametri=[{"EncodingType": "HotEncoding", "LearningRate": 0.03, "Activatio
 
 csve = [{"PassengerId":1,"Survived":0,"Pclass":3,"Name":"Braund, Mr. Owen Harris","Sex":"male","Age":22,"SibSp":1,"Parch":0,"Ticket":"A/5 21171","Fare":7.25,"Cabin":"","Embarked":"S"},{"PassengerId":2,"Survived":1,"Pclass":1,"Name":"Cumings, Mrs. John Bradley (Florence Briggs Thayer)","Sex":"female","Age":38,"SibSp":1,"Parch":0,"Ticket":"PC 17599","Fare":71.2833,"Cabin":"C85","Embarked":"C"}]
 
+stats = [{"Count":3, "Unique":3, "Top":"f", "Freq":1, "Mean":2.0, "Std":1.0, "Min":1.0, "Q1":1.5, "Q2":2.0, "Q3":2.5, "Max":3.0}]
+
 @app.route("/users", methods=["POST"]) #Primanje sa beka
 def post_student():
     user = request.get_json()
@@ -43,6 +45,12 @@ def post_csv():
 @app.route("/csv", methods=['GET']) #Slanje CSV na bek
 def  getCsv():
     return jsonify(csvdata)
+
+@app.route("/stat", methods=["POST"]) #Primanje stata sa beka
+def post_stat():
+    stat = request.get_json()
+    stats.append(stat)
+    return stats
 
 
 #if(__name__=="main"):
