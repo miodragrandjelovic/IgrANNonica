fb9ceecedc36a737f2b21dd6e090af1a301ce5c6
Strahinja Tuovic <tuovicstrahinja@gmail.com>
2022-03-23 01:24:51 +0100

    Slanje hiperparametara

--- src/Front/src/app/home/home.component.ts
+++ src/Front/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
+import { Form, FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -18,10 +19,45 @@ export class HomeComponent implements OnInit {
     floor: 0,
     ceil: 100
   };
+
+  hyperparametersForm!: FormGroup;
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  get neuronControls() {
+   return (<FormArray>this.hyperparametersForm.get('neurons')).controls;
   }
+
+  ngOnInit(): void {
+    this.hyperparametersForm = new FormGroup({
+      'encodingType': new FormControl(null),
+      'learningRate': new FormControl(0),
+      'activation': new FormControl(null),
+      'epoch': new FormControl(0),
+      'regularization': new FormControl(null),
+      'regularizationRate': new FormControl(0),
+      'problemType': new FormControl(null),
+      'ratio': new FormControl(0),
+      'batchSize': new FormControl(0),
+      'randomize': new FormControl(0),
+      'neurons': new FormArray([]),
+    });
+  }
+
+  onSubmitHyperparameters() {
+    console.log(this.hyperparametersForm)
+  }
+
+  onAddLayer() {
+    const control = new FormControl(0);
+    (<FormArray>this.hyperparametersForm.get('neurons')).push(control);
+  }
+
+  onRemoveLayer(i:number) {
+    (<FormArray>this.hyperparametersForm.get('neurons')).removeAt(i);
+  }
+
     currentVal=0;
     getVal(val:any){
     console.warn(val)
