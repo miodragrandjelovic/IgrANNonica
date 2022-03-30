b837484732a8291e5fe47d6fec86d3fea2a6cf13
vukasinovic21 <nikolavukasinovic8@gmail.com>
2022-03-30 15:34:39 +0200

    izjednacavanje py sa devom

--- src/ML/ann/py.py
+++ src/ML/ann/py.py
@@ -1,4 +1,4 @@
-# PY FILE CALLS ALL THE STEPS FROM LINEAR FILE THAT ARE NEEDED FOR NEURAL NETWORK
+# PY FILE CALLS ALL THE STEPS FROM PROBLEM FILE THAT ARE NEEDED FOR NEURAL NETWORK
 
 
 # problem anna je to sto moze da nauci bilo sta
@@ -13,40 +13,37 @@
 
 
 from matplotlib.pyplot import hist
-#import linear as ln
-#import ann.linear as ln
-import ann.linear as ln
+#import problem as pr
+import ann.problem as pr
 import pandas as pd
 
 class Statistics():
     def __init__(self,type):
-        self.type = type
+        self.type = type    
         self.stats = None
 
-    def createLinear(self,train, features, label, epochs, ratio, activation_function, hidden_layers_n, hidden_layer_neurons_list, encode_type,randomize, batch_size, learning_rate, regularization, regularization_rate):
+    def createModel(self,train, features, label, epochs, ratio, activation_function, hidden_layers_n, hidden_layer_neurons_list, encode_type,randomize, batch_size, learning_rate, regularization, regularization_rate):
+        # model se kreira preko jedne fje, a u self.type se nalazi info da li treba da bude regresioni ili klasifikacioni
+        """
         data=None
-        data = ln.Data(train)
+        data =pr.Data(train)
         data.load_data(label, features)
-        data.clearupData(encode_type)
+        data.clearupData(encode_type,self.type)
         data.splitData(label, ratio, randomize, activation_function)
+        """
+
+        data=None
+        data=pr.Data(train)
+        data.Misa(encode_type,self.type,features,label,ratio,randomize)
+
 
         model=None
-        model = ln.Model(data,regularization, regularization_rate)
-        model.makeModel(activation_function, hidden_layers_n, hidden_layer_neurons_list,regularization,regularization_rate)
-        model.compileModel(learning_rate)
+        model =pr.Model(data,regularization, regularization_rate)
+        model.makeModel(self.type, activation_function, hidden_layers_n, hidden_layer_neurons_list,regularization,regularization_rate)
+        model.compileModel(self.type)
         model.trainModel(epochs,batch_size)
-      #  model.plotResults(epochs)
-        model.defMetrics()
+      #  model.plotResults(epochs, self.type)
+        model.defMetrics(self.type)
 
         self.stats = model.hist
         
-
-    def createCategorical(self,train, features, label, epochs, ratio, activation_function, hidden_layers_n, hidden_layer_neurons_list, encode_type,randomize, batch_size, learning_rate, regularization, regularization_rate):
-        pass
-
-    def createModel(self,train, features, label, epochs, ratio, activation_function, hidden_layers_n, hidden_layer_neurons_list, encode_type,randomize, batch_size, learning_rate, regularization, regularization_rate):
-        if (self.type == 'regression'):
-            self.createLinear(train, features, label, epochs, ratio, activation_function, hidden_layers_n, hidden_layer_neurons_list, encode_type,randomize, batch_size, learning_rate, regularization, regularization_rate)
-        elif (self.type == 'classification'):
-            self.createCategorical(train, features, label, epochs, ratio, activation_function, hidden_layers_n, hidden_layer_neurons_list, encode_type,randomize, batch_size, learning_rate, regularization, regularization_rate)
-
