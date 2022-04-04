e11d783d2c24d6691adbe0355ee73a431f07169f
lara.aksic <33-2018@pmf.kg.ac.rs>
2022-04-04 20:49:19 +0200

    dataset
    prikaz vec korisscenog dataseta odredjenog korisnika

--- src/Front/src/app/header/dataset/dataset.component.ts
+++ src/Front/src/app/header/dataset/dataset.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 import { CsvComponent } from 'src/app/home/csv/csv.component';
 
 @Component({
@@ -9,10 +10,44 @@ import { CsvComponent } from 'src/app/home/csv/csv.component';
 })
 export class DatasetComponent implements OnInit {
 
+  
+  map:Map<string, string[]>;
+  array2d: string[][]; 
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     
+    this.http.get<any>('https://localhost:7167/api/Python/csv').subscribe(result => {
+    
+
+    var map = new Map<string, string[]>();
+    
+    for(var i = 0; i < result.length; i++) {
+      if(i == 0) {
+        var aaa = Object.keys(result[i]);
+        for(var j = 0; j < aaa.length; j++) {
+          map.set(aaa[j], ["" + Object.values(result[i])[j]]);
+        }
+      } else {
+        var aaa = Object.keys(result[i]);
+        for(var j = 0; j < aaa.length; j++) {
+          var array = map.get(aaa[j]);
+          if(array == undefined) array = [];
+          array.push("" + Object.values(result[i])[j]);
+          map.set(aaa[j], array);
+        }
+      }
+      
+    }
+    console.log(map);
+    this.map=map;
+    this.array2d=Array.from(map.values());
+
+    this.array2d= this.array2d[0].map((_, colIndex) =>this.array2d.map(row => row[colIndex]));
+    
+  });
+ 
   }
 
   personImg:string="assets/images/person.jpg";
