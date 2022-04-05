fa05a59ee05c08811dce593aef10b9bc526ddc8a
vukasinovic21 <nikolavukasinovic8@gmail.com>
2022-04-05 16:05:53 +0200

    Na osnovu izabranog imena vraca se taj csv

--- src/Backend/Backend/Controllers/LoadData.cs
+++ src/Backend/Backend/Controllers/LoadData.cs
@@ -9,6 +9,12 @@ using System.Text.Json.Nodes;
 using System.Net.Http.Json;
 using Aspose.Cells;
 using Aspose.Cells.Utility;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+using System.Data;
+using LumenWorks.Framework.IO.Csv;
+using Newtonsoft.Json;
 
 namespace Backend.Controllers
 {
@@ -21,10 +27,10 @@ namespace Backend.Controllers
         public static string? Name { get; set; } //Ime ucitanog Csv fajla
         public static string? Username { get; set; } //Ulogovan korisnik
 
-        [HttpPost("savedCsv")] //Otvaranje foldera gde se nalazi izabrani csv
-        public async Task<ActionResult<Statistika>> PostSavedCsv(String name)
+        [HttpPost("selectedCsv")] //Otvaranje foldera gde se nalazi izabrani csv
+        public async Task<ActionResult<String>> PostSelectedCsv(String name)
         {
-
+            string fileName = name + ".csv";
             string CurrentPath = Directory.GetCurrentDirectory();
             string SelectedPath = CurrentPath + @"\Users\" + Username + "\\" + name;
             if(!System.IO.Directory.Exists(SelectedPath))
@@ -35,16 +41,34 @@ namespace Backend.Controllers
             {
                 return BadRequest("Niste ulogovani.");
             }
+
             string[] files = Directory.GetFiles(SelectedPath).Select(Path.GetFileName).ToArray();
 
-            return Ok(files);
+            string SelectedPaths = CurrentPath + @"\Users\" + Username + "\\" + name + "\\" + fileName;
+            /*var reader = new StreamReader(SelectedPaths);
+            var csv = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture);
+            List<JsonDocument> cList = csv.GetRecords<JsonDocument>().ToList();*/
+
+            var csvTable = new DataTable();
+            using (var csvReader = new LumenWorks.Framework.IO.Csv.CsvReader(new StreamReader(System.IO.File.OpenRead(SelectedPaths)), true))
+            {
+                csvTable.Load(csvReader);
+            }
+
+            string result = string.Empty;
+            result = JsonConvert.SerializeObject(csvTable);
+
+            var resultjson = System.Text.Json.JsonSerializer.Deserialize<JsonDocument>(result); //json
+            //var result = JsonSerializer.Deserialize<JsonElement>(csvTable); //json
+            //string result = JsonConvert.SerializeObject(DatatableToDictionary(queryResult, "Title"), Newtonsoft.Json.Formatting.Indented);
+            return Ok(resultjson);
         }
 
 
         [HttpPost("hp")] //Slanje HP na pajton
         public async Task<ActionResult<Hiperparametri>> Post([FromBody] Hiperparametri hiper)
         {
-            var hiperjson = JsonSerializer.Serialize(hiper);
+            var hiperjson = System.Text.Json.JsonSerializer.Serialize(hiper);
             var data = new StringContent(hiperjson, System.Text.Encoding.UTF8, "application/json");
             var url = "http://127.0.0.1:3000/hp";
             var response = await http.PostAsync(url, data);
@@ -67,7 +91,7 @@ namespace Backend.Controllers
             //                                                                                  model
 
             HttpResponseMessage httpResponse = await http.GetAsync("http://127.0.0.1:3000/model");
-            var model = JsonSerializer.Deserialize<JsonDocument>(await httpResponse.Content.ReadAsStringAsync()); 
+            var model = System.Text.Json.JsonSerializer.Deserialize<JsonDocument>(await httpResponse.Content.ReadAsStringAsync()); 
             var dataModel = await httpResponse.Content.ReadAsStringAsync(); 
 
             var workbook = new Workbook();
@@ -107,7 +131,7 @@ namespace Backend.Controllers
             var response = await http.PostAsync(url, data);
 
             HttpResponseMessage httpResponse = await http.GetAsync("http://127.0.0.1:3000/stats");
-            var stat = JsonSerializer.Deserialize<JsonDocument>(await httpResponse.Content.ReadAsStringAsync());
+            var stat = System.Text.Json.JsonSerializer.Deserialize<JsonDocument>(await httpResponse.Content.ReadAsStringAsync());
 
             string currentPath = Directory.GetCurrentDirectory();
             var upgradedName = name.Substring(0, name.Length - 4);
@@ -145,7 +169,7 @@ namespace Backend.Controllers
         [HttpPost("stats")] //Slanje Stats na pajton
         public async Task<ActionResult<Statistika>> PostStat(Statistika stat)
         {
-            var statjson = JsonSerializer.Serialize(stat);
+            var statjson = System.Text.Json.JsonSerializer.Serialize(stat);
             var data = new StringContent(statjson, System.Text.Encoding.UTF8, "application/json");
             var url = "http://127.0.0.1:3000/stats";
             var response = await http.PostAsync(url, data);
