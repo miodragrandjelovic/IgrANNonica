4b6fad9297d006199578de0676c392dbe59f5270
Strahinja Tuovic <tuovicstrahinja@gmail.com>
2022-04-21 14:01:26 +0200

    Povezivanje funkcionalnosti u toku - Heatmap

--- src/Front/src/app/home/heatmap/heatmap.component.ts
+++ src/Front/src/app/home/heatmap/heatmap.component.ts
@@ -30,7 +30,7 @@ export class HeatmapComponent implements OnChanges {
     name: 'myScheme',
     selectable: true,
     group: ScaleType.Time,
-    domain: ['#69add5','#ffffff', '#69add5'],
+    domain: ['#F6F3F3', '#69add5'],
   };
 
   labels:any;
@@ -42,25 +42,24 @@ export class HeatmapComponent implements OnChanges {
 
     this.datasets = [];
     this.labels = Object.keys(this.matrix);
-    console.log(this.labels);
-    console.log(this.matrix[this.labels[0]][this.labels[0]]);
     
     for (let i = 0; i < this.labels.length; i++) {
       const dataItems:DataItem[] = [];
       for(let j = this.labels.length - 1; j >= 0; j--){
-        const dataObject:DataItem = { name: this.labels[j], value:parseFloat(this.matrix[this.labels[i]][this.labels[j]]) }
+        const dataObject:DataItem = { name: this.labels[j], value:Math.abs(parseFloat(this.matrix[this.labels[i]][this.labels[j]])) }
         dataItems.push(dataObject);
       }
       this.datasets.push({ name: this.labels[i], series: dataItems });
     }
-    console.log(this.datasets);
   }
   onSelect(data:any): void {
     console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+
   }
   
   onActivate(data:any): void {
     console.log('Activate', JSON.parse(JSON.stringify(data)));
+    console.log(data);
   }
 
   onDeactivate(data:any): void {
