1728df95436419dd17a6451fe189e6bfce534f4e
Miodrag Randjelovic <dobropamtimsve00@gmail.com>
2022-05-25 13:17:20 +0200

    provereno i lokalno-uspesno radi

--- src/ML/pom2.py
+++ src/ML/pom2.py
@@ -11,6 +11,9 @@ from sklearn.preprocessing import LabelEncoder
 import category_encoders as ce
 
 
+dictionary=dict()
+
+
 df=pd.read_csv("src\ML\mpg.csv")
 
 df.head(10)
@@ -59,15 +62,69 @@ normalizer = layers.Normalization(axis=-1)
 normalizer.adapt(X_train)
 model.add(normalizer)
 """
-len(pom.columns)
 
 
-model.add(layers.Dense(units=32,input_shape=(len(X_train.columns),)))
+
+model.add(layers.Dense(units=32,input_shape=(X_train.shape[1],)))
 model.add(layers.Dense(units=32,activation='relu'))
 model.add(layers.Dense(units=64,activation='relu'))
-len(y_train.columns)
-model.add(layers.Dense(len(y_train.columns), activation="relu"))
+#len(y_train.columns)
+model.add(layers.Dense(y_train.shape[1], activation="relu"))
 
-model.compile(optimizer='adam', loss=MeanSquaredError(),metrics=['accuracy','mae','mse'])
+model.compile(optimizer='adam', loss=MeanSquaredError(),metrics=['mae','mse'])
 
 hist=model.fit(X_train, y_train, epochs=15,batch_size=10, validation_data = (X_test, y_test), verbose=1)
+
+dictionary['pera']=model
+
+dictionary
+
+
+model=None
+
+model=keras.Sequential()
+"""
+normalizer=None
+normalizer = layers.Normalization(axis=-1)
+normalizer.adapt(X_train)
+model.add(normalizer)
+"""
+
+
+
+model.add(layers.Dense(units=32,input_shape=(X_train.shape[1],)))
+model.add(layers.Dense(units=32,activation='relu'))
+model.add(layers.Dense(units=64,activation='relu'))
+#len(y_train.columns)
+model.add(layers.Dense(y_train.shape[1], activation="relu"))
+
+model.compile(optimizer='adam', loss=MeanSquaredError(),metrics=['mae','mse'])
+
+hist=model.fit(X_train, y_train, epochs=12,batch_size=10, validation_data = (X_test, y_test), verbose=1)
+
+dictionary['mile']=model
+
+
+model=None
+
+model=keras.Sequential()
+"""
+normalizer=None
+normalizer = layers.Normalization(axis=-1)
+normalizer.adapt(X_train)
+model.add(normalizer)
+"""
+
+
+
+model.add(layers.Dense(units=32,input_shape=(X_train.shape[1],)))
+model.add(layers.Dense(units=32,activation='relu'))
+model.add(layers.Dense(units=64,activation='relu'))
+#len(y_train.columns)
+model.add(layers.Dense(y_train.shape[1], activation="relu"))
+
+model.compile(optimizer='adam', loss=MeanSquaredError(),metrics=['mae','mse'])
+
+hist=model.fit(X_train, y_train, epochs=20,batch_size=10, validation_data = (X_test, y_test), verbose=1)
+
+dictionary['pera']=model
